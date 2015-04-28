ede0196a0e2b46aab88fa88e73627c59332c65a6
jltran <guardian@berkeley.edu>
2015-04-28 14:48:40 -0700

    changed settings.py turns out the problem was databases var was deleted...

--- erdos/settings.py
+++ erdos/settings.py
@@ -75,7 +75,12 @@ WSGI_APPLICATION = 'erdos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 # Move localDB to local_settings
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
