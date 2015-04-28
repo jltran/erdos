fafe3081e1fcfff6b6cb63dc8e86d0e0a3587f39
jltran <guardian@berkeley.edu>
2015-04-28 14:35:05 -0700

    change wsgi

--- erdos/erdos/wsgi.py
+++ erdos/erdos/wsgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erdos.settings")
-
-application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+from dj_static import Cling
+application = Cling(get_wsgi_application())
